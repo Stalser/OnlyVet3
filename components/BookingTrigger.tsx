29dfcea423019679c583e55193f7bb06dd2094cf
Данила <93940085+Stalser@users.noreply.github.com>
2025-11-14 14:20:33 +0500

    Add label prop to BookingTrigger component

--- components/BookingTrigger.tsx
+++ components/BookingTrigger.tsx
@@ -1,3 +1,11 @@
+"use client";
 
-import BookingModal from '@/components/BookingModal';
-export default function BookingTrigger(){return <BookingModal/>}
+import BookingModal from "@/components/BookingModal";
+
+type BookingTriggerProps = {
+  label?: string;
+};
+
+export default function BookingTrigger({ label = "Записаться" }: BookingTriggerProps) {
+  return <BookingModal triggerLabel={label} />;
+}
